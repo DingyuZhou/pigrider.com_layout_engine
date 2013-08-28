b1b06a6e11886411408671d0ca5fc3cd25244d79
pigRider <pigRider@pigRider.com>
2013-08-28 01:42:18 -0400

    [Develop]: Add social networks sharing functions.

--- app/controllers/pigrider_layout/main_controller.rb
+++ app/controllers/pigrider_layout/main_controller.rb
@@ -10,5 +10,11 @@ module PigriderLayout
   
     def contactUs
     end
+    
+    def socialNetworkSharing
+      respond_to do |format|
+        format.html {render :layout=>false}
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PigriderLayout::Engine.routes.draw do
   match "AboutUs"=>"main#aboutUs", :as=>:aboutUs
-  match "ContactUs"=>"main#contactUs", :as=>:contactUs 
+  match "ContactUs"=>"main#contactUs", :as=>:contactUs
+  post "socialNetworkSharing"=>"main#socialNetworkSharing", :as=>:socialNetworkSharing
 end
